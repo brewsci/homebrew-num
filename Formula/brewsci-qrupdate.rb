24102c0ad5272e71e80dcf275cc3226b5542e784
Dominique Orban <dominique.orban@gmail.com>
2018-10-25 19:06:46 -0400

    brewsci-qrupdate 1.1.2 (new formula)

---
+++ Formula/brewsci-qrupdate.rb
@@ -0,0 +1,35 @@
+class BrewsciQrupdate < Formula
+  desc "Fast updates of QR and Cholesky decompositions"
+  homepage "https://sourceforge.net/projects/qrupdate/"
+  url "https://downloads.sourceforge.net/qrupdate/qrupdate-1.1.2.tar.gz"
+  sha256 "e2a1c711dc8ebc418e21195833814cb2f84b878b90a2774365f0166402308e08"
+
+  depends_on "gcc"
+  depends_on "openblas"
+
+  def install
+    # Parallel compilation not supported. Reported on 2017-07-21 at
+    # https://sourceforge.net/p/qrupdate/discussion/905477/thread/d8f9c7e5/
+    ENV.deparallelize
+
+    system "make", "lib", "solib", "FC=gfortran",
+                   "BLAS=-L#{Formula["openblas"].opt_lib} -lopenblas"
+
+    # Confuses "make install" on case-insensitive filesystems
+    rm "INSTALL"
+
+    # BSD "install" does not understand GNU -D flag.
+    # Create the parent directory ourselves.
+    inreplace "src/Makefile", "install -D", "install"
+    lib.mkpath
+
+    system "make", "install", "PREFIX=#{prefix}"
+    pkgshare.install "test/tch1dn.f", "test/utils.f"
+  end
+
+  test do
+    system "gfortran", "-o", "test", pkgshare/"tch1dn.f", pkgshare/"utils.f",
+                   "-L#{lib}", "-lqrupdate", "-L#{Formula["openblas"].opt_lib}", "-lopenblas"
+    assert_match "PASSED   4     FAILED   0", shell_output("./test")
+  end
+end
