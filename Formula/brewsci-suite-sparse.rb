2687cc239bde4905a9a60bc166865f20d18bb853
Dominique Orban <dominique.orban@gmail.com>
2018-10-25 23:13:49 -0400

    brewsci-suite-sparse 4.5.5 (new formula)
    Closes #16.
    
    Signed-off-by: Dominique Orban <dominique.orban@gmail.com>

---
+++ Formula/brewsci-suite-sparse.rb
@@ -0,0 +1,32 @@
+class BrewsciSuiteSparse < Formula
+  desc "Suite of Sparse Matrix Software"
+  homepage "http://faculty.cse.tamu.edu/davis/suitesparse.html"
+  url "http://faculty.cse.tamu.edu/davis/SuiteSparse/SuiteSparse-4.5.5.tar.gz"
+  sha256 "b9a98de0ddafe7659adffad8a58ca3911c1afa8b509355e7aa58b02feb35d9b6"
+
+  keg_only "suite-sparse is provided by homebrew/core"
+
+  depends_on "brewsci/num/brewsci-metis"
+  depends_on "openblas"
+
+  def install
+    args = [
+      "INSTALL=#{prefix}",
+      "BLAS=-L#{Formula["openblas"].opt_lib} -lopenblas",
+      "LAPACK=$(BLAS)",
+      "MY_METIS_LIB=-L#{Formula["brewsci-metis"].opt_lib} -lmetis",
+      "MY_METIS_INC=#{Formula["brewsci-metis"].opt_include}",
+    ]
+    system "make", "library", *args
+    system "make", "install", *args
+    lib.install Dir["**/*.a"]
+    pkgshare.install "KLU/Demo/klu_simple.c"
+  end
+
+  test do
+    ENV.prepend_path "LD_LIBRARY_PATH", lib unless OS.mac?
+    system ENV.cc, "-o", "test", "-I#{include}", pkgshare/"klu_simple.c",
+                   "-L#{lib}", "-lsuitesparseconfig", "-lklu"
+    system "./test"
+  end
+end
