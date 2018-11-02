e93688d3d3443525c76d581f453a776a4f357b4c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-11-02 12:50:17 +0000

    starpu: add 1.2.6 bottle.

--- Formula/starpu.rb
+++ Formula/starpu.rb
@@ -4,6 +4,12 @@ class Starpu < Formula
   url "http://starpu.gforge.inria.fr/files/starpu-1.2.6/starpu-1.2.6.tar.gz"
   sha256 "eb67a7676777b6ed583722aca5a9f63145b842f390ac2f5b3cbc36fe910d964c"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-num"
+    sha256 "eb9757a75eeb7f82d1c5baffcfcd6dc6e95a7c23a8f840d47e2e1b91741dd88e" => :sierra
+    sha256 "7b0450cf223d60fd11bec88cadb77aa12c754168a86bfb5a1c3b8461e9d93d7e" => :x86_64_linux
+  end
+
   head do
     url "https://scm.gforge.inria.fr/anonscm/git/starpu/starpu.git"
     depends_on "autoconf" => :build
