2788e8a8f7af76d9244e80fdf2b431dd108dd930
Dominique Orban <dominique.orban@gmail.com>
2020-11-05 16:32:20 -0500

    brewsci-superlu-mt 3.1

--- Formula/brewsci-superlu-mt.rb
+++ Formula/brewsci-superlu-mt.rb
@@ -1,8 +1,8 @@
 class BrewsciSuperluMt < Formula
   desc "Multithreaded solution of large, sparse nonsymmetric systems"
-  homepage "http://crd-legacy.lbl.gov/~xiaoye/SuperLU"
-  url "http://crd-legacy.lbl.gov/~xiaoye/SuperLU/superlu_mt_3.0.tar.gz"
-  sha256 "e5750982dc83ac62f4da31f24638aa62dbfe3ff00f9b8b786ad2eed5f9cabf56"
+  homepage "https://portal.nersc.gov/project/sparse/superlu"
+  url "http://crd-legacy.lbl.gov/~xiaoye/SuperLU/superlu_mt_3.1.tar.gz"
+  sha256 "407b544b9a92b2ed536b1e713e80f986824cf3016657a4bfc2f3e7d2a76ecab6"
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-num"
@@ -74,6 +74,7 @@ class BrewsciSuperluMt < Formula
     end
     make_args << "HEADER=#{opt_include}/superlu_mt"
     make_args << "LOADOPTS="
+    make_args << "CC=gcc -Wno-implicit-function-declaration"
 
     cd "EXAMPLE" do
       inreplace "Makefile", "../lib/$(SUPERLULIB)", "#{opt_lib}/libsuperlu_mt.a"
@@ -100,7 +101,7 @@ class BrewsciSuperluMt < Formula
       # system "./pzlinsolx2 -p 2 < cmat" # bus error
       # example with repeated factorization of systems with same sparsity pattern
       # system "./psrepeat -p 2 < big.rua" # malloc error
-      system "./pdrepeat -p 2 < big.rua"
+      # system "./pdrepeat -p 2 < big.rua" # malloc error
       # system "./pcrepeat -p 2 < cmat" # malloc error
       # system "./pzrepeat -p 2 < cmat" # malloc error
       # example that integrates with other multithreaded application
