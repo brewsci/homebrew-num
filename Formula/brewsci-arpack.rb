2830b724550dc04a9a8c17cd0c97c6fc227dbffc
Seeker <meaningseeking@protonmail.com>
2020-11-12 12:13:31 -0800

    Update brewsci-arpack.rb

--- Formula/brewsci-arpack.rb
+++ Formula/brewsci-arpack.rb
@@ -17,6 +17,7 @@ class BrewsciArpack < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
 
   depends_on "gcc"
   depends_on "openblas"
